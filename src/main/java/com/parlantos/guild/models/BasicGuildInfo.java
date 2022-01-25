200d3863b4c8ccb36ccc68e56d79e9ab5e4d1341
Johnathan Bressette <bressetteprogramming@gmail.com>
2022-01-24 19:11:30 -0500

    Fix basic guild info object to have lists of channels

--- src/main/java/com/parlantos/guild/models/BasicGuildInfo.java
+++ src/main/java/com/parlantos/guild/models/BasicGuildInfo.java
@@ -1,14 +1,15 @@
 package com.parlantos.guild.models;
 
 import java.math.BigInteger;
+import java.util.List;
 
 /**
  * Guild info object that contains the basic guild info for initially providing the display of all of the guilds
  */
 public class BasicGuildInfo {
 
-    TextChannelEntity textChannelEntity;
-    VoiceChannelEntity voiceChannelEntity;
+    List<TextChannelEntity> textChannelEntity;
+    List<VoiceChannelEntity> voiceChannelEntity;
     int currentTextChannelIndex;
     String name;
     String icon;
