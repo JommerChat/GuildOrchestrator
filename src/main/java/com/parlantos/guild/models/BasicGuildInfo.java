60ae4fc75709ce6fa555571591c4386966d2733f
Johnathan Bressette <bressetteprogramming@gmail.com>
2022-01-30 14:32:02 -0500

    Add getters and setters to BasicGuildInfo bean

--- src/main/java/com/parlantos/guild/models/BasicGuildInfo.java
+++ src/main/java/com/parlantos/guild/models/BasicGuildInfo.java
@@ -1,5 +1,7 @@
 package com.parlantos.guild.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigInteger;
 import java.util.List;
 
@@ -8,11 +10,67 @@ import java.util.List;
  */
 public class BasicGuildInfo {
 
-    List<TextChannelEntity> textChannelEntity;
-    List<VoiceChannelEntity> voiceChannelEntity;
-    int currentTextChannelIndex;
-    String name;
-    String icon;
-    String description;
-    BigInteger id;
+    private List<TextChannelEntity> textChannelEntity;
+    private List<VoiceChannelEntity> voiceChannelEntity;
+    private int currentTextChannelIndex;
+    private String name;
+    private String icon;
+    private String description;
+    private BigInteger id;
+
+    public List<TextChannelEntity> getTextChannelEntity() {
+        return textChannelEntity;
+    }
+
+    public void setTextChannelEntity(List<TextChannelEntity> textChannelEntity) {
+        this.textChannelEntity = textChannelEntity;
+    }
+
+    public List<VoiceChannelEntity> getVoiceChannelEntity() {
+        return voiceChannelEntity;
+    }
+
+    public void setVoiceChannelEntity(List<VoiceChannelEntity> voiceChannelEntity) {
+        this.voiceChannelEntity = voiceChannelEntity;
+    }
+
+    public int getCurrentTextChannelIndex() {
+        return currentTextChannelIndex;
+    }
+
+    public void setCurrentTextChannelIndex(int currentTextChannelIndex) {
+        this.currentTextChannelIndex = currentTextChannelIndex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
 }
