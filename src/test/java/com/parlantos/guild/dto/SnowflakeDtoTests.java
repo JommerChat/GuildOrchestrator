d8d3caa90641719a2e9480a28248e2aaef7c8c0c
Johnathan Bressette <bressetteprogramming@gmail.com>
2022-03-04 16:26:27 -0500

    Add test for the snowflake dto

--- src/test/java/com/parlantos/guild/dto/SnowflakeDtoTests.java
+++ src/test/java/com/parlantos/guild/dto/SnowflakeDtoTests.java
@@ -1,2 +1,40 @@
-package com.parlantos.guild.dto;public class SnowflakeDtoTests {
+package com.parlantos.guild.dto;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SnowflakeDtoTests {
+
+    private static SnowflakeDto snowflakeDto;
+
+    public static MockWebServer mockWebServer;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeAll
+    static void setUp() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+        snowflakeDto = new SnowflakeDto("http://localhost:" + mockWebServer.getPort() + "/guild/data");
+    }
+
+    @Test
+    public void testGetSnowflakeId() throws JsonProcessingException {
+        BigInteger mockSnowflakeValue = new BigInteger("1234");
+        mockWebServer.enqueue(new MockResponse()
+                .setBody(objectMapper.writeValueAsString(mockSnowflakeValue))
+                .addHeader("Content-Type", "application/json"));
+        BigInteger result = snowflakeDto.getSnowflakeId().block();
+        assertEquals(mockSnowflakeValue, result);
+    }
+
 }
